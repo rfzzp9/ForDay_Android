bd80518f1ea209bbabd331c3a96763d2e3bc0961
chaewon gwak <rfzzp9@gmail.com>
2025-12-25 12:27:38 +0900

    feat : 패키지 구조화 및 build.gradle.kts 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,13 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.verify.detekt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.baselineprofile) apply false
+    alias(libs.plugins.roborazzi.plugin) apply false
 }
