2bb682017bce68efb257f7ec884c0189753cddbf
chaewon gwak <rfzzp9@gmail.com>
2026-02-02 02:17:32 +0900

    Feat : Theme 파일 생성

---
+++ app/src/main/java/com/forday/app/core/designsystem/theme/Theme.kt
@@ -0,0 +1,57 @@
+package com.forday.app.core.designsystem.theme
+
+import android.app.Activity
+import android.view.WindowManager
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@Composable
+fun ForDayTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    if (!LocalInspectionMode.current) {
+        val view = LocalView.current
+
+        SideEffect {
+            val window = (view.context as? Activity)?.window ?: return@SideEffect
+            val insetsController = WindowCompat.getInsetsController(window, view)
+
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+
+            insetsController.isAppearanceLightStatusBars = !darkTheme
+            insetsController.isAppearanceLightNavigationBars = !darkTheme
+
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalAppTypography provides LocalTypography,
+        LocalAppColor provides FordayColor,
+        LocalAppGradients provides ForDayGradients,
+        content = content,
+    )
+}
+
+object ForDayTheme {
+    val typography: FordayTypography
+        @Composable
+        get() = LocalAppTypography.current
+
+    val color: FordayColor
+        @Composable
+        get() = LocalAppColor.current
+
+    val gradients: ForDayGradients
+        @Composable
+        get() = LocalAppGradients.current
+}
